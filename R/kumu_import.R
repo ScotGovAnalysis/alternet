cee0f48c0b321721d2aac20aa1091e65dd547108
Millson D (David) <David.Millson@gov.scot>
2023-08-09 14:32:34 +0100

    Feature: Create function to import network from a json file exported from Kumu.

---
+++ R/kumu_import.R
@@ -0,0 +1,38 @@
+import_from_kumu_json = function() {
+  json_data = jsonlite::fromJSON("inst/extdata/kumu-davidmillson-softor.json")
+
+  nodes = json_data$elements %>%
+    tidyr::unnest(cols = c(attributes)) %>%
+    dplyr::rename(element = `_id`, refno = id) %>%
+    dplyr::left_join(json_data$maps$elements[[1]] %>%
+                       tidyr::unnest(cols = c(position, style)),
+                     by = "element") %>%
+    dplyr::rename(name = element,
+                  type = `element type`,
+                  id = `_id`,
+                  font_colour = fontColor,
+                  font_weight = fontWeight)
+
+  styles = nodes %>%
+    dplyr::distinct(type, font_colour, font_weight)
+
+  nodes = nodes %>%
+    dplyr::select(-pinned, -font_colour, -font_weight)
+
+  edges = json_data$connections %>%
+    tidyr::unnest(cols = c(attributes)) %>%
+    dplyr::rename(connection = `_id`, refno = id) %>%
+    dplyr::left_join(tibble::as_tibble(json_data$maps$connections[[1]]),
+                     by = "connection") %>%
+    dplyr::select(name = connection,
+                  refno,
+                  polarity = `connection type`,
+                  from,
+                  to,
+                  id = `_id`,
+                  curvature) %>%
+    dplyr::mutate(polarity = polarity %>%
+                    dplyr::recode(`+` = "positive", `-` = "negative"))
+
+  list(nodes = nodes, edges = edges, styles = styles)
+}
