b795dcbc9588f7accb0288e4b47c33d6030aaa4f
Millson D (David) <David.Millson@gov.scot>
2023-08-23 13:25:59 +0100

    Bug: Change import_from_decision_explorer_xml to produce a network in the correct format to match for testing

--- R/decision_explorer_import.R
+++ R/decision_explorer_import.R
@@ -11,16 +11,21 @@ import_from_decision_explorer_xml = function(filepath, scaling = 5) {
 
   nodes = xml_data %>%
     get_node_info_de() %>%
-    dplyr::mutate(id = stringr::str_c("elem-", refno)) %>% # create a unique id for the node, based on the refno
+    dplyr::mutate(name = stringr::str_c("elem-", refno), # create a unique id for the node, based on the refno
+                  id = stringr::str_c("node-", refno)) %>%
     # rescale coordinates to keep layout nice
-    dplyr::mutate(x = multiply_chr(x, 1/scaling),
-                  y = multiply_chr(y, -1/scaling)) # also reverse y direction as decision explorer has origin in bottom left rather than top left
+    dplyr::mutate(x = x / scaling,
+                  y = -y / scaling) %>%  # also reverse y direction as decision explorer has origin in bottom left rather than top left
+    dplyr::select(name, refno, label, type, id, x, y)
 
   edges = xml_data %>%
     get_edge_info_de() %>%
     dplyr::mutate_at(c("from", "to"), ~stringr::str_c("elem-", .)) %>% # specifying from and to nodes by id instead of refno
     dplyr::mutate(refno = 1:nrow(.),# assign a unique refno for the edge
-                  id = stringr::str_c("conn-", refno)) # create a unique id for the edge, based on the refno
+                  name = stringr::str_c("conn-", refno),
+                  id = stringr::str_c("edge-", refno), # create a unique id for the edge, based on the refno
+                  curvature = as.double(NA)) %>%
+    dplyr::select(name, refno, polarity, from, to, id, curvature)
 
   list(nodes = nodes, edges = edges) # return the node and edge information
 }
@@ -36,13 +41,13 @@ get_node_info_de = function(raw_xml) {
 
   layout_xml = xml2::xml_find_all(raw_xml, ".//position") # Navigate to the position elements
 
-  nodes = tibble::tibble(refno = xml2::xml_attr(nodes_xml, "id"),
+  nodes = tibble::tibble(refno = xml2::xml_attr(nodes_xml, "id") %>% as.integer(),
                          label = xml2::xml_text(nodes_xml),
                          type = xml2::xml_attr(nodes_xml, "style"))
 
-  layout = tibble::tibble(x = xml2::xml_attr(layout_xml, "x"),
-                          y = xml2::xml_attr(layout_xml, "y"),
-                          refno = xml2::xml_attr(layout_xml, "concept"))
+  layout = tibble::tibble(x = xml2::xml_attr(layout_xml, "x") %>% as.double(),
+                          y = xml2::xml_attr(layout_xml, "y") %>% as.double(),
+                          refno = xml2::xml_attr(layout_xml, "concept") %>% as.integer())
 
   nodes %>%
     dplyr::left_join(layout, by = "refno")
