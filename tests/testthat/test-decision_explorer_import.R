f6202a4ccb49d31e0abf5a7e40277a43cef95a40
Millson D (David) <David.Millson@gov.scot>
2023-08-23 13:47:59 +0100

    Bug: Change name of import_from_decision_explorer_xml test

--- tests/testthat/test-decision_explorer_import.R
+++ tests/testthat/test-decision_explorer_import.R
@@ -1,4 +1,4 @@
-test_that("export_to_decision_explorer works", {
+test_that("import_from_decision_explorer_xml works", {
   expect_equal(alternet::example_network,
                import_from_decision_explorer_xml("inst/extdata/example_network.mdx"))
 })
