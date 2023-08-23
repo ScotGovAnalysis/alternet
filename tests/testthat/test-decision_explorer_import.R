d97284712fd9efa22ddef489a4170c26042e749c
Millson D (David) <David.Millson@gov.scot>
2023-08-23 13:41:40 +0100

    Bug: Fix tests to access the package data

--- tests/testthat/test-decision_explorer_import.R
+++ tests/testthat/test-decision_explorer_import.R
@@ -1,4 +1,4 @@
 test_that("export_to_decision_explorer works", {
-  expect_equal(example_network,
+  expect_equal(alternet::example_network,
                import_from_decision_explorer_xml("inst/extdata/example_network.mdx"))
 })
