7836706a8a88a97b529c1fa97334187cb1f5f9c7
Millson D (David) <David.Millson@gov.scot>
2023-08-25 15:23:20 +0100

    Feature: Add function to export network to a json for importing into Kumu

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -4,3 +4,22 @@ multiply_chr = function(x, factor) {
     `*`(factor) %>%
     as.character()
 }
+
+nest_for_json = function(data, ...) {
+  data %>%
+    tidyr::nest(...) %>%
+    purrr::pmap(listify_elements) %>%
+    jsonlite:::simplify()
+}
+
+listify_elements = function(...) {
+  list(...) %>%
+    purrr::map_if(tibble::is_tibble, simplify_to_list)
+}
+
+simplify_to_list = function(df) {
+  x = df %>%
+    as.list()
+
+  x[! x %>% purrr::map_lgl(is.na)]
+}
